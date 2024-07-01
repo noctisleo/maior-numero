0a97a4c3db8a415631ea6b665f1619d1c9fadeec
Leachenski <jcleachenski@gmail.com>
2024-07-01 20:56:33 -0300

    feat: adiciona validação e calculo do maior número possível

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const prompt = require("prompt-sync")();
 
-const numero = prompt("Digite um número: ")
+function ehNumero(num) {
+  return !isNaN(Number(num));
+}
+
+function maiorNumero(num) {
+  if (ehNumero(num)) {
+    let array = num.split("");
+
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[i] < array[j]) {
+          let aux = array[j];
+          array[j] = array[i];
+          array[i] = aux;
+        }
+      }
+    }
+
+    return array.join("");
+  }
+  console.log("Número inválido");
+}
+
+const numero = prompt("Digite um número: ");
+
+let maior = maiorNumero(numero);
+console.log(maior);
